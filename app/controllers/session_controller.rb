0c7a1b88510390f2a333b0264fba327429b7c0c4
Vanya <iwan.germanow@gmail.com>
2014-05-13 11:34:12 +0300

    add welcome page

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,16 +1,19 @@
 class SessionController < ApplicationController
-before_filter :logout , except: [:destroy]
+
+  before_filter :logout , except: [:destroy]
   
   def new
+   @user = User.new
   end
 
   def create
-  	user = User.find_by_login(params[:login]) 
-  	if user and user.authenticate(params[:password])
-  		session[:user_id] = user.id
-  		redirect_to user_path(user)
+
+  	@user = User.find_by_login(params[:user][:login]) 
+  	if @user and @user.authenticate(params[:user][:password])
+  		session[:user_id] = @user.id
+  		redirect_to user_path(@user)
     else
-  		redirect_to session_new_path , notice: "Wrong login or password"
+  		redirect_to session_new_path , alert: "Wrong login or password"
   	end
   end
 
@@ -18,4 +21,6 @@ before_filter :logout , except: [:destroy]
   	session[:user_id] = nil
   	redirect_to session_new_path
   end
+
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,13 +27,14 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
+    
     @user = User.new(user_params)
     respond_to do |format|
       if @user.save
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render action: 'show', status: :created, location: @user }
       else
-        format.html { render action: 'new' }
+        format.html { render action: 'new' , alert: "Fields can't be blank "}
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
